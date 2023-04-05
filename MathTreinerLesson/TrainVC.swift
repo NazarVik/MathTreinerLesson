36227f8007dbd80a44c728781ccded3abd921baf
NazarVik <Nazarov_dev@icloud.com>
2023-04-05 23:06:00 +0300

    home work

--- MathTreinerLesson/TrainVC.swift
+++ MathTreinerLesson/TrainVC.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 final class TrainViewController : UIViewController {
+    
+@IBOutlet var buttonsCollection: [UIButton]!
+
     // MARK: - Properties
     var type: MathTypes = .add {
         didSet {
             print(type)
         }
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configButtons()
+        
+    }
+    
+    
+    private func configButtons(){
+        buttonsCollection.forEach{button in
+            button.layer.shadowColor = UIColor.darkGray.cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 3)
+            button.layer.shadowOpacity = 0.5
+            button.layer.shadowRadius = 4
+            
+        }
+    }
 }
+
